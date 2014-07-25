fd3ef90cbae21399e11b5a9cd01ab623e4fa4c50
Dario Vilardi <vilardid@posteitaliane.it>
2014-07-25 14:39:10 +0200

    minor fix make.sh

--- make.sh
+++ make.sh
@@ -16,12 +16,12 @@ $BINECHO "Carico le procedure di input";
 $BINMYSQL -umagazzino -pmagauser -D magazzino -e 'source sp_inp.sql';
 $BINECHO "Carico le APIs pubbliche";
 $BINMYSQL -umagazzino -pmagauser -D magazzino -e 'source sp_pub.sql';
-$BINECHO "Carico i dati";
-$BINMYSQL -umagazzino -pmagauser -D magazzino -e 'source dati.sql';
 $BINECHO "Carico le viste sui dati";
 $BINMYSQL -umagazzino -pmagauser -D magazzino -e 'source view.sql';
 $BINECHO "Carico le procedure di aggiornamento dati";
 $BINMYSQL -umagazzino -pmagauser -D magazzino -e 'source sp_upd.sql';
+$BINECHO "Carico i dati";
+$BINMYSQL -umagazzino -pmagauser -D magazzino -e 'source dati.sql';
 
 $BINECHO "Carico le viste per il service";
 $BINMYSQL -umagazzino -pmagauser -D magazzino -e 'source vserv.sql';
