e5efdf0bb4f105d637e8f3a3d3f237c73c315ea1
Dario Vilardi <vilardid@posteitaliane.it>
2014-10-02 12:34:23 +0200

    fix warning code 1329

--- sp.sql
+++ sp.sql
@@ -2,7 +2,7 @@ USE magazzino;
 
 -- ---------------------- tokenizza tags ----------------------
 DELIMITER //
--- DROP PROCEDURE IF EXISTS tokenizza_tags //
+DROP PROCEDURE IF EXISTS tokenizza_tags //
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `tokenizza_tags`(IN tokens TEXT)
 BEGIN
 DECLARE i INT;
@@ -14,9 +14,8 @@ myloop: LOOP
 	SET @i = @i + 1;
 	IF (@token = '') THEN
 		LEAVE myloop;
-	ELSE
-		CALL input_proprieta('1',@token);
 	END IF;
+	CALL input_proprieta('1',@token);
 END LOOP myloop;
 
 END //
@@ -24,28 +23,32 @@ DELIMITER ;
 
 
 -- ---------------------- fix proprieta tags ----------------------
--- note: genera un warning
+-- note: genera(va) un warning // FIXATO!
+-- DECLARE CONTINUE HANDLER FOR NOT FOUND SET @done = TRUE;
 -- | Error | 1329 | No data - zero rows fetched, selected, or processed |
 DELIMITER //
--- DROP PROCEDURE IF EXISTS fix_proprieta_tags //
+DROP PROCEDURE IF EXISTS fix_proprieta_tags //
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `fix_proprieta_tags`()
 BEGIN
 
-DECLARE done INT DEFAULT FALSE;
-DECLARE in_tags TEXT;
-
-DECLARE foo CURSOR FOR SELECT tags FROM MERCE;
-DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-OPEN foo;
-bar: LOOP 
-	FETCH foo INTO in_tags;
-	IF done THEN
-		LEAVE bar;
-	END IF;
-	CALL tokenizza_tags(in_tags);
-END LOOP bar;
-CLOSE foo;
+	DECLARE done INT DEFAULT FALSE;
+	DECLARE cursor_val TEXT;
+	DECLARE cursor_i CURSOR FOR SELECT tags FROM MERCE;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND
+    BEGIN
+		SELECT 1 INTO @done FROM (SELECT 1) AS t;
+    END;
+    
+	OPEN cursor_i;
+	
+	read_loop: LOOP 
+		FETCH cursor_i INTO cursor_val;
+		IF @done THEN
+			LEAVE read_loop;
+		END IF;
+		CALL tokenizza_tags(cursor_val);
+	END LOOP;
+	CLOSE cursor_i;
 
 END //
 DELIMITER ;
