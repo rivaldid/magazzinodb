4070f6c279344058b42686bf2f08413f3b0c6b81
Dario Vilardi <vilardid@posteitaliane.it>
2014-11-19 19:14:20 +0100

    working@delta_magazzino

--- sp_upd.sql
+++ sp_upd.sql
@@ -1,5 +1,8 @@
+USE magazzino;
+
+
 DELIMITER //
-DROP PROCEDURE IF EXISTS upd_instestazione_registro //
+-- DROP PROCEDURE IF EXISTS upd_instestazione_registro//
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `upd_instestazione_registro`( 
 IN in_id_registro INT,
 IN in_contatto VARCHAR(45)
@@ -15,7 +18,7 @@ DELIMITER ;
 
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS upd_giacenza_magazzino //
+-- DROP PROCEDURE IF EXISTS upd_giacenza_magazzino//
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `upd_giacenza_magazzino`(
 IN in_utente VARCHAR(45),
 IN in_id_merce INT,
@@ -32,8 +35,8 @@ IF (SELECT EXISTS(SELECT 1 FROM MAGAZZINO WHERE id_merce=in_id_merce AND posizio
 SET temp_quantita=(SELECT quantita FROM MAGAZZINO WHERE id_merce=in_id_merce AND posizione=in_posizione);
 SET temp_tags=(SELECT tags FROM MERCE WHERE id_merce=in_id_merce);
 
-CALL SCARICO('Sistema','Aggiornamento giacenze sistema',in_id_merce,temp_quantita,in_posizione,in_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento giacenza magazzino',@myvar);
-CALL CARICO(in_utente,'Aggiornamento giacenze sistema','Sistema',(SELECT next_system_doc()),in_data,NULL,temp_tags,in_quantita,in_posizione,in_data,'Carico invocato dal sistema per aggiornamento giacenza magazzino',NULL,NULL);
+CALL SCARICO(in_utente,'Aggiornamento',in_id_merce,temp_quantita,in_posizione,in_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento giacenza magazzino',@myvar);
+CALL CARICO(in_utente,'Aggiornamento','Sistema',(SELECT next_system_doc()),in_data,NULL,temp_tags,in_quantita,in_posizione,in_data,'Carico invocato dal sistema per aggiornamento giacenza magazzino',NULL,NULL);
 
 END IF;
 
@@ -43,7 +46,7 @@ DELIMITER ;
 
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS upd_posizione_magazzino //
+-- DROP PROCEDURE IF EXISTS upd_posizione_magazzino//
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `upd_posizione_magazzino`( 
 IN in_utente VARCHAR(45),
 IN in_id_merce INT,
@@ -60,8 +63,8 @@ IF (SELECT EXISTS(SELECT 1 FROM MAGAZZINO WHERE id_merce=in_id_merce AND posizio
 SET temp_quantita=(SELECT quantita FROM MAGAZZINO WHERE id_merce=in_id_merce AND posizione=in_vecchia_posizione);
 SET temp_tags=(SELECT tags FROM MERCE WHERE id_merce=in_id_merce);
 
-CALL SCARICO('Sistema','Aggiornamento posizioni sistema',in_id_merce,temp_quantita,in_vecchia_posizione,in_vecchia_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento posizione magazzino',@myvar);
-CALL CARICO(in_utente,'Aggiornamento posizioni sistema','Sistema',(SELECT next_system_doc()),in_data,NULL,temp_tags,temp_quantita,in_nuova_posizione,in_data,'Carico invocato dal sistema per aggiornamento posizione magazzino',NULL,NULL);
+CALL SCARICO(in_utente,'Aggiornamento',in_id_merce,temp_quantita,in_vecchia_posizione,in_vecchia_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento posizione magazzino',@myvar);
+CALL CARICO(in_utente,'Aggiornamento','Sistema',(SELECT next_system_doc()),in_data,NULL,temp_tags,temp_quantita,in_nuova_posizione,in_data,'Carico invocato dal sistema per aggiornamento posizione magazzino',NULL,NULL);
 
 END IF;
 
@@ -70,7 +73,7 @@ DELIMITER ;
 
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS upd_doc_carico //
+-- DROP PROCEDURE IF EXISTS upd_doc_carico//
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `upd_doc_carico`( 
 IN in_id_operazioni INT,
 IN in_fornitore VARCHAR(45),
@@ -91,11 +94,11 @@ IF (in_id_operazioni IS NOT NULL) THEN
 	IF (SELECT EXISTS(SELECT 1 FROM OPERAZIONI WHERE id_operazioni = in_id_operazioni)) THEN
 		
 		UPDATE OPERAZIONI SET id_registro=@my_id_registro WHERE id_operazioni=in_id_operazioni;
-		SET @ritorno := 0;
+		SET @ritorno = 0;
 		
 	ELSE	
 		
-		SET @ritorno := 1;
+		SET @ritorno = 1;
 		
 	END IF;
 	
@@ -107,21 +110,17 @@ DELIMITER ;
 
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS delta_magazzino_001 //
+-- DROP PROCEDURE IF EXISTS delta_magazzino_001//
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `delta_magazzino_001`( 
 IN in_utente VARCHAR(45),
 IN in_id_merce INT,
 IN in_posizione VARCHAR(45),
 IN in_1st_quantita INT,
 IN in_2nd_quantita INT,
-IN in_data DATE,
-OUT ritorno INT
+IN in_data DATE
 ) 
 BEGIN
 DECLARE diff_quantita INT;
-DECLARE result_scarico INT;
-
-SET @ritorno:=1;
 
 IF (SELECT EXISTS(SELECT 1 FROM MAGAZZINO WHERE id_merce=in_id_merce AND posizione=in_posizione AND quantita=in_1st_quantita)) THEN
 
@@ -131,12 +130,8 @@ IF (SELECT EXISTS(SELECT 1 FROM MAGAZZINO WHERE id_merce=in_id_merce AND posizio
 
 		IF (@diff_quantita>=0) THEN
 		
-			CALL SCARICO(in_utente,'Aggiornamento giacenze sistema',in_id_merce,@diff_quantita,in_posizione,in_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento giacenze magazzino',@result_scarico);
-			
-			-- IF (@result_scarico = 0) THEN
-				CALL CARICO(in_utente,'Aggiornamento giacenze magazzino','Sistema',(SELECT next_system_doc()),in_data,NULL,(SELECT tags FROM MERCE WHERE id_merce=in_id_merce),in_2nd_quantita,in_posizione,in_data,'Carico invocato dal sistema per aggiornamento giacenze magazzino',NULL,NULL);
-				SET @ritorno:=0;
-			-- END IF;
+			CALL SCARICO(in_utente,'Aggiornamento',in_id_merce,@diff_quantita,in_posizione,in_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento giacenze magazzino',@myvar);
+			CALL CARICO(in_utente,'Aggiornamento','Sistema',(SELECT next_system_doc()),in_data,NULL,(SELECT tags FROM MERCE WHERE id_merce=in_id_merce),in_2nd_quantita,in_posizione,in_data,'Carico invocato dal sistema per aggiornamento giacenze magazzino',NULL,NULL);
 		
 		END IF;  -- end test quantita
 		
@@ -144,46 +139,56 @@ IF (SELECT EXISTS(SELECT 1 FROM MAGAZZINO WHERE id_merce=in_id_merce AND posizio
 
 END IF; -- end esistenza
 
-SELECT @ritorno AS 'risultato';
-
 END //
 DELIMITER ;
 
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS delta_magazzino_010 //
+-- DROP PROCEDURE IF EXISTS delta_magazzino_010//
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `delta_magazzino_010`( 
 IN in_utente VARCHAR(45),
 IN in_id_merce INT,
 IN in_1st_posizione VARCHAR(45),
 IN in_2nd_posizione VARCHAR(45),
 IN in_quantita INT,
-IN in_data DATE,
-OUT ritorno INT
+IN in_data DATE
 ) 
 BEGIN
-DECLARE result_scarico INT;
-
-SET @ritorno:=1;
 
 IF (SELECT EXISTS(SELECT 1 FROM MAGAZZINO WHERE id_merce=in_id_merce AND posizione=in_1st_posizione AND quantita=in_quantita)) THEN
 
-	CALL SCARICO(in_utente,'Aggiornamento posizioni sistema',in_id_merce,in_quantita,in_1st_posizione,in_1st_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento posizioni magazzino',@result_scarico);
-			
-	-- IF (@result_scarico = 0) THEN
-	CALL CARICO(in_utente,'Aggiornamento posizioni magazzino','Sistema',(SELECT next_system_doc()),in_data,NULL,(SELECT tags FROM MERCE WHERE id_merce=in_id_merce),in_quantita,in_2nd_posizione,in_data,'Carico invocato dal sistema per aggiornamento posizioni magazzino',NULL,NULL);
-	-- END IF;
+CALL SCARICO(in_utente,'Aggiornamento',in_id_merce,in_quantita,in_1st_posizione,in_1st_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento posizioni magazzino',@myvar);
+CALL CARICO(in_utente,'Aggiornamento','Sistema',(SELECT next_system_doc()),in_data,NULL,(SELECT tags FROM MERCE WHERE id_merce=in_id_merce),in_quantita,in_2nd_posizione,in_data,'Carico invocato dal sistema per aggiornamento posizioni magazzino',NULL,NULL);
 
 END IF;
-
-SELECT @ritorno AS 'risultato';
-
 END //
 DELIMITER ;
 
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS delta_magazzino //
+-- DROP PROCEDURE IF EXISTS delta_magazzino_100//
+CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `delta_magazzino_100`( 
+IN in_utente VARCHAR(45),
+IN in_1st_id_merce INT,
+IN in_2nd_id_merce INT,
+IN in_posizione VARCHAR(45),
+IN in_quantita INT,
+IN in_data DATE
+) 
+BEGIN
+
+IF (SELECT EXISTS(SELECT 1 FROM MAGAZZINO WHERE id_merce=in_1st_id_merce AND posizione=in_posizione AND quantita=in_quantita)) THEN
+
+CALL SCARICO(in_utente,'Aggiornamento',in_1st_id_merce,in_quantita,in_posizione,in_posizione,in_data,in_data,'Scarico invocato dal sistema per aggiornamento merce magazzino',@myvar);
+CALL CARICO(in_utente,'Aggiornamento','Sistema',(SELECT next_system_doc()),in_data,NULL,(SELECT tags FROM MERCE WHERE id_merce=in_2nd_id_merce),in_quantita,in_posizione,in_data,'Carico invocato dal sistema per aggiornamento merce magazzino',NULL,NULL);
+
+END IF;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+-- DROP PROCEDURE IF EXISTS delta_magazzino//
 CREATE DEFINER=`magazzino`@`localhost` PROCEDURE `delta_magazzino`(
 IN in_utente VARCHAR(45),
 IN in_1st_tags TEXT,
@@ -198,10 +203,8 @@ IN in_data DATE
 ) 
 BEGIN
 
-DECLARE ret_001 INT;
-DECLARE ret_010 INT;
 
--- se ho tags restituisco id
+-- pre: mi assicuro di lavorare con id
 IF (in_1st_tags IS NOT NULL) THEN
 	CALL input_merce(in_1st_tags,in_1st_id_merce);
 END IF;
@@ -209,21 +212,31 @@ END IF;
 IF (in_2nd_tags IS NOT NULL) THEN
 	CALL input_merce(in_2nd_tags,in_2nd_id_merce);
 END IF;
--- return id
+
+
+
+-- in base alla terna id-posizione-quantita, smisto alla sp designata
 
 IF ((in_1st_id_merce IS NOT NULL) AND (in_1st_posizione IS NOT NULL) AND (in_1st_quantita IS NOT NULL)) THEN
 
 -- 001
 IF ((in_2nd_id_merce IS NULL) AND (in_2nd_posizione IS NULL) AND (in_2nd_quantita IS NOT NULL)) THEN
-CALL delta_magazzino_001(in_utente,in_1st_id_merce,in_1st_posizione,in_1st_quantita,in_2nd_quantita,in_data,@ret_001);		
+CALL delta_magazzino_001(in_utente,in_1st_id_merce,in_1st_posizione,in_1st_quantita,in_2nd_quantita,in_data);		
 END IF;
 
 -- 010
 IF ((in_2nd_id_merce IS NULL) AND (in_2nd_posizione IS NOT NULL) AND (in_2nd_quantita IS NULL)) THEN
-CALL delta_magazzino_010(in_utente,in_1st_id_merce,in_1st_posizione,in_2nd_posizione,in_1st_quantita,in_data,@ret_010);	
+CALL delta_magazzino_010(in_utente,in_1st_id_merce,in_1st_posizione,in_2nd_posizione,in_1st_quantita,in_data);	
+END IF;
+
+-- 100
+IF ((in_2nd_id_merce IS NOT NULL) AND (in_2nd_posizione IS NULL) AND (in_2nd_quantita IS NULL)) THEN
+CALL delta_magazzino_100(in_utente,in_1st_id_merce,in_2nd_id_merce,in_1st_posizione,in_1st_quantita,in_data);	
 END IF;
 
 END IF; -- end test valori
+
+
 
 END //
 DELIMITER ;
