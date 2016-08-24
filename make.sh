9255a756c0e3c28821b113895c6549e303e2c545
vilardid <vilardid@posteitaliane.it>
2016-08-24 11:53:38 +0200

    without data

--- make.sh
+++ make.sh
@@ -12,11 +12,11 @@ tracefile=$PREFIX2/temp_trace.sql
 BINMYSQL="/usr/bin/mysql"
 BINDUMP="/usr/bin/mysqldump"
 
-BINCD="/usr/bin/cd"
-BINECHO="/usr/bin/echo"
-BINTOUCH="/usr/bin/touch"
-BINDATE="/usr/bin/date"
-BINRM="/usr/bin/rm"
+BINCD="cd"
+BINECHO="/bin/echo"
+BINTOUCH="/bin/touch"
+BINDATE="/bin/date"
+BINRM="/bin/rm"
 
 A="<h3> -->"
 B="</h3>"
@@ -24,11 +24,14 @@ C="<P>"
 D="</P>"
 
 foo() {
-while read -r line ; do $BINECHO $C.$line.$D >> $logfile; done
+while read -r line ; do $BINECHO $C $line $D >> $logfile; done
 }
 
 MYARGS="-H -umagazzino -pmagauser -D magazzino"
-#MYARGS="--login-path=local -D magazzino"
+MYARGS1="-H -uroot -p"
+
+#MYARGS="--login-path=magazzino -D magazzino"
+#MYARGS1="--login-path=root"
 
 $BINCD $PREFIX
 $BINRM $logfile
@@ -41,6 +44,10 @@ $BINECHO "<h1>Eseguito il $($BINDATE +"%d/%m/%Y %H.%M.%S")</h1>" >> $logfile
 
 $BINECHO $A "Backup del watchdog" $B >> $logfile
 $BINDUMP -umagazzino -pmagauser magazzino trace > $tracefile
+#$BINDUMP --login-path=magazzino magazzino trace > $tracefile
+
+$BINECHO $A "Utenza con relativi permessi" $B >> $logfile
+$BINMYSQL $MYARGS1 -e "source ${PREFIX}/make_magauser.sql \W;" | foo
 
 $BINECHO $A "Carico la base" $B >> $logfile
 $BINMYSQL $MYARGS -e "source ${PREFIX}/base.sql \W;" | foo
@@ -72,17 +79,17 @@ $BINMYSQL $MYARGS -e "source ${PREFIX}/debug.sql \W;" | foo
 
 $BINECHO -ne '#############             (66%)\r'
 
-$BINECHO $A "Carico i dati" $B >> $logfile
+#$BINECHO $A "Carico i dati" $B >> $logfile
 #$BINMYSQL $MYARGS -e "source ${PREFIX}/dati.sql \W;" >> $logfile
-pv "${PREFIX}/dati.sql" | $BINMYSQL $MYARGS >> $logfile
+#pv "${PREFIX}/dati.sql" | $BINMYSQL $MYARGS | foo
 
-if [ -f ${PREFIX2}/database.sql ]; then
+#if [ -f ${PREFIX2}/database.sql ]; then
 #$BINMYSQL $MYARGS -e "source ${PREFIX2}/database.sql \W;" >> $logfile
-pv "${PREFIX2}/database.sql" | $BINMYSQL $MYARGS >> $logfile
-fi
+#pv "${PREFIX2}/database.sql" | $BINMYSQL $MYARGS | foo
+#fi
 
 $BINECHO -ne '#######################   (100%)\r'
 
 $BINECHO $A "Restore del watchdog" $B >> $logfile
-$BINMYSQL $MYARGS -e "source $tracefile \W;" >> $logfile
+$BINMYSQL $MYARGS -e "source $tracefile \W;" | foo
 $BINRM $tracefile
